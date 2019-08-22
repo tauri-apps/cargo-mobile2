3472a673e7c97297fe931eaad2b0917f52eaa0fe
Francesca Plebani <francesca@brainiumstudios.com>
2019-08-21 17:24:12 -0700

    Fix Android project gen for new config structure

--- ginit/src/android/project.rs
+++ ginit/src/android/project.rs
@@ -1,24 +1,39 @@
 use super::target::Target;
 use crate::{config::Config, target::TargetTrait as _, templating::template_pack, util};
 use into_result::command::CommandError;
-use std::fs;
+use std::{fs, path::PathBuf};
 
 #[derive(Debug, derive_more::From)]
 pub enum ProjectCreationError {
-    MissingTemplatePack,
+    MissingTemplatePack {
+        name: &'static str,
+    },
     TemplateProcessingError(bicycle::ProcessingError),
+    CreateDirError {
+        tried_to_create: PathBuf,
+        error: std::io::Error,
+    },
     SymlinkAssetsError(CommandError),
-    CreateDirError(std::io::Error),
 }
 
 // TODO: We should verify Android env vars / offer defaults
 pub fn create(config: &Config, bike: &bicycle::Bicycle) -> Result<(), ProjectCreationError> {
-    let src = template_pack(Some(config), "android_studio_project")
-        .ok_or_else(|| ProjectCreationError::MissingTemplatePack)?;
+    let src = template_pack(Some(config), "android_studio_project").ok_or_else(|| {
+        ProjectCreationError::MissingTemplatePack {
+            name: "android_studio_project",
+        }
+    })?;
     let dest = config.android().project_path();
     bike.process(src, &dest, |map| {
         config.insert_template_data(map);
-        map.insert("abi_list", {
+        map.insert(
+            "abi_list",
+            Target::all()
+                .values()
+                .map(|target| target.abi)
+                .collect::<Vec<_>>(),
+        );
+        map.insert("abi_list_joined", {
             Target::all()
                 .values()
                 .map(|target| format!("\"{}\"", target.abi))
@@ -27,7 +42,10 @@ pub fn create(config: &Config, bike: &bicycle::Bicycle) -> Result<(), ProjectCre
         });
     })?;
     let dest = dest.join("app/src/main/assets/");
-    fs::create_dir_all(&dest)?;
+    fs::create_dir_all(&dest).map_err(|error| ProjectCreationError::CreateDirError {
+        tried_to_create: dest.clone(),
+        error,
+    })?;
     util::relative_symlink(config.asset_path(), dest)?;
     Ok(())
 }
