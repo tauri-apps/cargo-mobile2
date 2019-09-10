2bfdf29d97abbe93a11c994020ab5d9cf5ea15e2
Francesca Plebani <francesca@brainiumstudios.com>
2019-09-10 16:06:27 -0700

    Forward `SSH_AUTH_SOCK` env var so cargo git deps will work

--- ginit/src/env.rs
+++ ginit/src/env.rs
@@ -10,6 +10,7 @@ pub struct Env {
     home: String,
     path: String,
     term: Option<String>,
+    ssh_auth_sock: Option<String>,
 }
 
 impl Env {
@@ -17,7 +18,13 @@ impl Env {
         let home = std::env::var("HOME").map_err(EnvError::HomeNotSet)?;
         let path = std::env::var("PATH").map_err(EnvError::PathNotSet)?;
         let term = std::env::var("TERM").ok();
-        Ok(Self { home, path, term })
+        let ssh_auth_sock = std::env::var("SSH_AUTH_SOCK").ok();
+        Ok(Self {
+            home,
+            path,
+            term,
+            ssh_auth_sock,
+        })
     }
 }
 
@@ -26,6 +33,9 @@ impl ExplicitEnv for Env {
         let mut env = vec![("HOME", self.home.as_ref()), ("PATH", self.path.as_ref())];
         if let Some(term) = self.term.as_ref() {
             env.push(("TERM", term.as_ref()));
+        }
+        if let Some(ssh_auth_sock) = self.ssh_auth_sock.as_ref() {
+            env.push(("SSH_AUTH_SOCK", ssh_auth_sock.as_ref()));
         }
         env
     }
