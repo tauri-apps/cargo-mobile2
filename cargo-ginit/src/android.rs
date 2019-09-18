44192c75f61f9db19c9a52c579b6cf45198265c3
Francesca Plebani <francesca@brainiumstudios.com>
2019-09-18 15:42:12 -0700

    Use --non-interactive on CI

--- cargo-ginit/src/android.rs
+++ cargo-ginit/src/android.rs
@@ -123,32 +123,6 @@ impl AndroidCommand {
 
     pub fn exec(self, config: &Config, noise_level: NoiseLevel) -> Result<(), Error> {
         detect_device!(adb::device_list, Android);
-        // fn detect_device<'a>(env: &'_ Env) -> Result<Device<'a>, Error> {
-        //     let device_list = adb::device_list(env).map_err(Error::DeviceDetectionFailed)?;
-        //     if device_list.len() > 0 {
-        //         let index = if device_list.len() > 1 {
-        //             prompt::list(
-        //                 "Detected Android devices",
-        //                 device_list.iter(),
-        //                 "device",
-        //                 None,
-        //                 "Device",
-        //             )
-        //             .map_err(Error::DevicePromptFailed)?
-        //         } else {
-        //             0
-        //         };
-        //         let device = device_list.into_iter().nth(index).unwrap();
-        //         println!(
-        //             "Detected connected device: {} with target {:?}",
-        //             device,
-        //             device.target().triple,
-        //         );
-        //         Ok(device)
-        //     } else {
-        //         Err(Error::NoDevicesDetected)
-        //     }
-        // }
 
         fn detect_target_ok<'a>(env: &Env) -> Option<&'a Target<'a>> {
             detect_device(env).map(|device| device.target()).ok()
