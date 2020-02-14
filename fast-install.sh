a50acac0677f5df35587d7a22e38e66b6725b8dc
Francesca Plebani <francesca@brainiumstudios.com>
2020-02-14 12:04:38 -0800

    fast-install can optionally install only a specific plugin

--- fast-install.sh
+++ fast-install.sh
@@ -6,8 +6,16 @@ function install {
     cargo build -p $1 && cp target/debug/$2$1 ~/.cargo/bin/$2$1
 }
 
-install "ginit" "cargo-"
-install "ginit-android"
-install "ginit-brainium"
-install "ginit-ios"
+if [ -z "$1" ]; then
+    install "ginit" "cargo-"
+    install "ginit-android"
+    install "ginit-brainium"
+    install "ginit-ios"
+else
+    if [ "$1" = "ginit" ]; then
+        install "ginit" "cargo-"
+    else
+        install "ginit-$1"
+    fi
+fi
 
