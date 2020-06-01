c1b6837e64b62fdd631e7697a1940bd8e44acb32
Megan Worley <megan@brainiumstudios.com>
2020-06-01 16:48:25 -0700

    Fix issues when running under new ci setup

--- ci/install.sh
+++ ci/install.sh
@@ -4,9 +4,4 @@ set -ex
 
 export RUST_BACKTRACE=1
 
-BRANCH_NAME="${1:?}"
-DEV_BRANCH="${2:?}"
-
-if [[ $BRANCH_NAME == $DEV_BRANCH ]]; then
-    cargo install --path . --force
-fi
+cargo install --path . --force
--- ci/test_project.sh
+++ ci/test_project.sh
@@ -4,19 +4,10 @@ set -ex
 
 export RUST_BACKTRACE=1
 
-BRANCH_NAME="${1:?}"
-DEV_BRANCH="${2:?}"
-
-EXE_PATH_MOBILE="cargo mobile"
-EXE_PATH_ANDROID="cargo android"
-EXE_PATH_APPLE="cargo apple"
-
-if [ $BRANCH_NAME != $DEV_BRANCH ]; then
-    cargo build
-    EXE_PATH_MOBILE=$(realpath "target/debug/cargo-mobile")
-    EXE_PATH_ANDROID=$(realpath "target/debug/cargo-android")
-    EXE_PATH_APPLE=$(realpath "target/debug/cargo-apple")
-fi
+cargo build
+EXE_PATH_MOBILE=$(realpath "target/debug/cargo-mobile")
+EXE_PATH_ANDROID=$(realpath "target/debug/cargo-android")
+EXE_PATH_APPLE=$(realpath "target/debug/cargo-apple")
 
 # test by building a temporary project
 rm -rf ./tmp
