a6fd14b0515304a327451c5fbf3984b4a20c0463
Francesca Lovebloom <francesca@brainiumstudios.com>
2020-08-17 12:45:11 -0700

    Add comments in fast install script

--- fast-install.sh
+++ fast-install.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# This script is only intended for use during development. It's faster than a
+# regular `cargo install`, which makes iteration more pleasant.
+
 set -ex
 
 function copy {
     cp target/debug/cargo-$1 ~/.cargo/bin/cargo-$1
 }
 
+# TODO: using this feature here makes this script less useful for anyone outside
+# the company
 cargo build -p cargo-mobile --features brainium
 copy "android"
 copy "apple"
