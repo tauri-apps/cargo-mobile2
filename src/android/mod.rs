238a2100f246b14bce942517ed0b926eb28e08a4
Lucas Nogueira <lucas@tauri.studio>
2022-08-15 08:21:06 -0300

    feat: expose modules

--- src/android/mod.rs
+++ src/android/mod.rs
@@ -1,13 +1,13 @@
 pub(crate) mod adb;
 mod bundletool;
 pub mod cli;
-pub(crate) mod config;
+pub mod config;
 mod device;
-pub(crate) mod env;
+pub mod env;
 mod jnilibs;
-mod ndk;
+pub mod ndk;
 pub(crate) mod project;
 mod source_props;
-mod target;
+pub mod target;
 
 pub static NAME: &str = "android";
