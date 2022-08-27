e4b9363fade30f065d631d172339a4b95a04ea3a
Ludea <ludovicw35@hotmail.com>
2022-08-27 14:30:56 +0200

    android support

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -6,10 +6,10 @@ mod macos;
 #[cfg(target_os = "macos")]
 pub use self::macos::*;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "android"))]
 mod linux;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "android"))]
 pub use self::linux::*;
 
 #[cfg(windows)]
@@ -18,7 +18,7 @@ mod windows;
 #[cfg(windows)]
 pub use self::windows::*;
 
-#[cfg(not(any(target_os = "macos", target_os = "linux", windows)))]
+#[cfg(not(any(target_os = "macos", target_os = "android", target_os = "linux", windows)))]
 compile_error!("Host platform not yet supported by cargo-mobile! We'd love if you made a PR to add support for this platform ❤️");
 
 // TODO: we should probably expose common functionality throughout `os` in a
