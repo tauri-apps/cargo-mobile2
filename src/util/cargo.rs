821885f71f77f777450b046798181c62cfe209d4
Lucas Nogueira <lucas@tauri.app>
2023-04-22 17:52:37 -0300

    undo SDKROOT

--- src/util/cargo.rs
+++ src/util/cargo.rs
@@ -115,8 +115,6 @@ impl<'a> CargoCommand<'a> {
         }
 
         duct::cmd("cargo", args)
-            // with the SDKROOT env var we get the `using sys root for 'iPhoneSimulator' but targeting 'MacOSX'` error
-            .env_remove("SDKROOT")
             .vars(env.explicit_env())
             .vars(explicit_cargo_env())
     }
