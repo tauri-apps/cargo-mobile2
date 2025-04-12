bbbbdf71c69748d18e7daa543f613f3cf5301b47
Lucas Fernandes Nogueira <lucas@tauri.app>
2025-04-12 14:24:05 -0300

    refactor(apple): fix bundle version validation (#450)
    * refactor(apple): fix bundle version validation
    
    bundle version can actually contain one to three period-separated integers, so our validation is incorrect
    
    see https://developer.apple.com/documentation/bundleresources/information-property-list/cfbundleversion
    
    * add bundle_version_short getter

--- src/apple/cli.rs
+++ src/apple/cli.rs
@@ -341,9 +341,10 @@ impl Exec for Input {
                     &detect_target_ok,
                     &env,
                     |target: &Target| {
-                        let mut app_version = config.bundle_version().clone();
+                        let mut app_version = config.bundle_version().to_string();
                         if let Some(build_number) = build_number {
-                            app_version.push_extra(build_number);
+                            app_version.push('.');
+                            app_version.push_str(&build_number.to_string());
                         }
 
                         target
