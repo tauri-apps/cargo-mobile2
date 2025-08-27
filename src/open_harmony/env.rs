d639bd8cfd4ef682bda94f511b94bd87155f8b37
Lucas Nogueira <lucas@tauri.app>
2025-08-27 12:23:27 -0300

    set OHOS_NDK_HOME for cargo-ohos

--- src/open_harmony/env.rs
+++ src/open_harmony/env.rs
@@ -88,6 +88,10 @@ impl ExplicitEnv for Env {
             "OHOS_HOME".into(),
             self.ohos_home.as_os_str().to_os_string(),
         );
+        envs.insert(
+            "OHOS_NDK_HOME".into(),
+            self.ohos_home.as_os_str().to_os_string(),
+        );
         envs
     }
 }
