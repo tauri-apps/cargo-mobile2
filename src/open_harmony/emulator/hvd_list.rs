b26dda2b04c1306fb0156599d3ef0f289419e6ee
Lucas Nogueira <lucas@tauri.app>
2025-08-27 10:38:32 -0300

    fix windows emulator list path

--- src/open_harmony/emulator/hvd_list.rs
+++ src/open_harmony/emulator/hvd_list.rs
@@ -11,9 +11,20 @@ pub enum Error {
 }
 
 pub fn hvd_list() -> Result<BTreeSet<Emulator>, Error> {
+    #[cfg(windows)]
+    let list_config_path = dirs::data_local_dir()
+        .unwrap()
+        .join("Huawei/Emulator/deployed/lists.json");
+    #[cfg(not(windows))]
     let list_config_path = dirs::home_dir()
         .unwrap()
         .join(".Huawei/Emulator/deployed/lists.json");
+
+    if !list_config_path.exists() {
+        log::error!("HVD list file not found: {}", list_config_path.display());
+        return Ok(BTreeSet::new());
+    }
+
     let hvd_json = std::fs::read_to_string(list_config_path).map_err(Error::ReadHvdFailed)?;
     serde_json::from_str(&hvd_json).map_err(Error::ParseHvdFailed)
 }
